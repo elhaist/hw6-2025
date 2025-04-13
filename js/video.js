264f1984d0e342c136feb80994a86fba95638abb
elhaist <elhaist@umich.edu>
2025-04-13 17:57:13 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -7,8 +7,9 @@ window.addEventListener("load", function() {
 	video.loop=false;
 	video.volume = 1.0;
 	video.load();
+	
 	document.querySelector("#volume").innerHTML = (video.volume * 100) +"%";
-	this.document.querySelector("#slider").value = video.volume * 100;
+	document.querySelector("#slider").value = video.volume * 100;
 });
 
 document.querySelector("#play").addEventListener("click", function() {
